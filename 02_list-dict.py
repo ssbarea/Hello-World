a798d56ef1d3f3b055b4b5998e2d1739c6f004a4
Samuel Sanchez <samuelsanchezbarea@gmail.com>
2019-02-01 19:56:46 +0100

    Create 02_list-dict.py
    New Excercise

---
+++ 02_list-dict.py
@@ -0,0 +1,15 @@
+lista1 = ["Hello", "world", "Cisco"]
+lista2 = [1, 2, 3]
+lista3 = ["CCNA", "Digitaliza", 4, 5, 2.0, 2.1]
+
+var1 = lista1[2]
+var2 = lista2[2]
+var3 = lista3[2]
+
+diccio = {'autor':'Joss Whedon', 'peli':'vengadores'}
+
+print(lista1, lista2, lista3)
+print("Estos son los últimos valores de cada lista:", var1, var2, var3)
+print(diccio)
+print(diccio.keys())
+print(diccio.values())
