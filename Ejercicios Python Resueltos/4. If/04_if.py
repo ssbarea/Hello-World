41e46b51208edfc0af2c31606122cb83b2f3f659
Samuel Sanchez <samuelsanchezbarea@gmail.com>
2019-02-03 12:13:40 +0100

    4. if excercise

---
+++ Ejercicios Python Resueltos/4. If/04_if.py
@@ -0,0 +1,10 @@
+i= int(input("Por favor, introduzca un número:"))
+
+if i < 20 :
+       print("El número",i,"se encuentra en el rango < 20")
+elif i >= 20 and i < 40:
+       print("El número",i,"se encuentra en el rango de 20 a 39")
+elif i >= 40 and i < 60:
+       print("El número",i,"se encuentra en el rango de 40 a 59")
+elif i >= 60:
+       print("El número",i,"se encuentra en el rango >= 60")
