3ac982259340e7a3ad7673a6f11cb749d1ebbf62
Samuel Sanchez <samuelsanchezbarea@gmail.com>
2019-02-03 10:36:39 +0100

    New Exercise

---
+++ Ejercicios Python Resueltos/3. Info personal/03_personal-info.py
@@ -0,0 +1,7 @@
+name=input("Nombre:")
+surname=input("Apellido:")
+phone=input("Teléfono:")
+city=input("Ciudad:")
+age=input("Edad:")
+
+print("Bienvenidx",name,surname,"\nsu teléfono es:",str(phone),"\nsu ciudad es:",city,"\ny su edad es:",str(age))
